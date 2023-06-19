bc2324852610ede78eb65565efa0546ed0d2244b
Rodrigo Costa <rodrigo.costa94@yahoo.com.br>
2023-06-18 21:51:32 -0300

    correção do bug do contador acelerado

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,6 +4,9 @@ import React, { useEffect, useState } from "react";
 
 function Counter (){
 
+    const [displayMiliSeconds, setDisplayMiliSeconds] = useState(0)
+    const [miliSeconds, setMIliSeconds] = useState(0)
+
     const [displaySeconds, setDisplaySeconds] = useState(0)
     const [seconds, setSeconds] = useState(0)
 
@@ -14,23 +17,32 @@ function Counter (){
     const [hours, setHours] = useState(0)
 
     const [intervalo, setIntervalo] = useState()
-    const [validador, setValidador] = useState(null)
 
+    let lap
+    // let isPlaying = false
 
     useEffect(() => {
         timerFunction()
-    },[seconds])
+    },[miliSeconds])
 
-    function timerFunction(){             
-        if (seconds == 10){
-            setDisplaySeconds("")
+    function timerFunction(){   
+        
+        //Mili Seconds
+        if (miliSeconds >= 10){
+            setDisplayMiliSeconds("")
+        }else if(miliSeconds < 10){
+            setDisplayMiliSeconds("0")
         }
-        if (minutes == 10){
-            setDisplayMinutes("")
+
+        if (miliSeconds == 100){
+            setDisplayMiliSeconds(0)
+            setMIliSeconds(0)
+            setSeconds((prevSeconds) => prevSeconds + 1)
+            // setMIliSeconds((prevMinutes) => prevMinutes + 1  )
+            setDisplaySeconds(0)     
         }
-        if (hours == 10){
-            setDisplayHours("")
-        }
+        
+        //Seconds
         if (seconds >= 10){
             setDisplaySeconds("")
         }else if(seconds < 10){
@@ -41,57 +53,94 @@ function Counter (){
             setMinutes((prevMinutes) => prevMinutes + 1  )
             setDisplaySeconds(0)     
         }
+
+        //Minutes
+        if (minutes == 10){
+            setDisplayMinutes("")
+        }
         if (minutes == 60){
             setMinutes(0)
             setHours((prevHours) => prevHours + 1  )
             setDisplayMinutes(0)
-        }        
+        }    
+
+        //Hours
+        if (hours == 10){
+            setDisplayHours("")
+        }    
     }
 
     function increment(){
-        setSeconds((prevSeconds) => prevSeconds + 1  )
+        // setSeconds((prevSeconds) => prevSeconds + 1  )
+        setMIliSeconds((prevMiliSeconds) => prevMiliSeconds + 1  )
     }
 
-    //Arrumar validador para não aumentar velocidade do contador apos ser clicado mais de 1 vez
-    function pause(){
-        if(validador){
-            clearInterval(intervalo)
-            clearInterval(validador)
-            setValidador(null)
-        }
-        
-    }
+    // function playPause(){
 
-        //Arrumar validador para não aumentar velocidade do contador apos ser clicado mais de 1 vez
-    function play(){
-        if(!validador){
-            setValidador(
+    //     isPlaying? play() : pause()
+    // }
+
+    function play(){      
+
+        // Elimina a execução em paralelo de outras funções contadoras, assim DEIXANDO DE ACELERAR O CONTADOR
+        if(!intervalo){
             setIntervalo(setInterval(() => {
                 increment()
                 timerFunction()
-            }, 1000))   )
+            }, 100))   
         }
-         
+
+        // isPlaying = true
+        
+        // setIntervalo(setInterval(() => {
+        //     increment()
+        //     timerFunction()
+        // }, 1000))
+    }
+
+    function pause(){       
+        
+        // Elimina a execução em paralelo de outras funções contadoras, assim DEIXANDO DE ACELERAR O CONTADOR
+        if(intervalo){
+            clearInterval(intervalo)
+            setIntervalo()
+        }
+
+        // isPlaying = false
+        // clearInterval(intervalo)
     }
 
     function reset(){
         pause()
+        setMIliSeconds(0)
+        setDisplayMiliSeconds(0)
         setSeconds(0)
         setDisplaySeconds(0)
         setMinutes(0)
         setDisplayMinutes(0)
         setHours(0)
         setDisplayHours(0)
+        setIntervalo()
         timerFunction()
+    }
+
+    function lapFunction(){
+
+        lap = ("" + displayHours + hours + ":" + displayMinutes + minutes + ":" + displaySeconds + seconds + ":" + displayMiliSeconds + miliSeconds)
+
+        console.log(lap)        
+        
     }
 
 
     return(
         <div>
-            <h1>{displayHours}{hours} : {displayMinutes}{minutes} : {displaySeconds}{seconds}</h1>
+            <h1>{displayHours}{hours} : {displayMinutes}{minutes} : {displaySeconds}{seconds} : {displayMiliSeconds}{miliSeconds}</h1>
             <button onClick={play}>Play</button>
             <button onClick={pause} >Pause</button>
+            {/* <button onClick={playPause} >Play</button> */}
             <button onClick={reset} >Reset</button>
+            <button onClick={lapFunction}> Lap </button>
         </div>
     )
     
